badc093e98f9e03a8201ed3698cee97a5238dfbb
MarcoAdami <smarco.adami@gmail.com>
2026-07-15 08:34:04 +0200

    feat: add enter key event for updating the expression for text box

--- src/components/input_panel.rs
+++ src/components/input_panel.rs
@@ -1,11 +1,19 @@
 // file: src/components/input_panel.rs
-use leptos::prelude::*;
+use leptos::{ev::KeyboardEvent, prelude::*};
 
 use crate::state::AppState;
 
 #[component]
 pub fn InputPanel() -> impl IntoView {
     let state = AppState::use_state();
+
+    let handle_key = move |ev: KeyboardEvent| {
+        if ev.key() == "Enter" {
+            // Your logic here
+            state.parse_expression()
+            
+        }
+    };
 
     view! {
         <div class="input-panel">
@@ -14,6 +22,7 @@ pub fn InputPanel() -> impl IntoView {
                 class="expression-input"
                 prop:value={state.input_text}
                 on:input={move |ev| state.set_input_text.set(event_target_value(&ev))}
+                on:keydown=handle_key
             />
             <button on:click={move |_| state.parse_expression()}>Parse & Build Tree</button>
             <button on:click={move |_| state.solve_selected()}>Solve Selected Leaves</button>
