8f92038178bd02988f92906b8259c58da52559de
MarcoAdami <smarco.adami@gmail.com>
2026-09-12 22:17:50 +0200

    refactor: delete useless configuration

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-// file: src/main.rs
 mod app;
 mod components;
 mod state;
@@ -6,7 +5,6 @@ mod state;
 use app::App;
 use leptos::mount::mount_to_body;
 
-#[cfg(not(feature = "csr"))]
 fn main() {
     console_error_panic_hook::set_once();
     mount_to_body(|| leptos::view! { <App /> });
